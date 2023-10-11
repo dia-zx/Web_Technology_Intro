6088c2e8f551a36e46c5a2b26b350834d1fa9b19
Ivan <dia_zx@mail.ru>
2023-10-11 20:30:10 +0300

    in process....

--- Lesson5/Chat/chat.php
+++ Lesson5/Chat/chat.php
@@ -3,7 +3,22 @@ $pdo = new PDO('sqlite:chat.db');
 if(isset($_POST['message']) && isset($_POST['user']) && !empty($_POST['message'] && !empty($_POST['user'])){
     $message = strip_tags($_POST['message']);
     $statement = $pdo->prepare(
-        'INSERT INTO messages (message)'
-    )
+        'INSERT INTO messages (user, message_text) VALUES (?, ?)'
+    );
+    $statement->execute([$_POST['user'], $message]);
+    $result = ['status' => "ok"];
+    echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    die();
 }
+
+$statement = $pdo->prepare(
+    'SELECT user, message_text FROM messages ORDER BY id DESC LIMIT 10;'
+);
+$statement->execute();
+$messages = $statement->fetchAll(PDO::FETCH_ASSOC);
+$result = [
+    'status' => "ok"
+    'messages' => $messages
+];
+echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
